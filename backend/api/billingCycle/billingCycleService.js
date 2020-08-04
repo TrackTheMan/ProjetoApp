6b2bfb659d10366dc7ac6770e4cb661ef06ee33f
Rafael <agostinetti@windowslive.com>
2020-08-04 18:05:20 -0300

    Uniformizando mensagens de erro

--- backend/api/billingCycle/billingCycleService.js
+++ backend/api/billingCycle/billingCycleService.js
@@ -1,17 +1,28 @@
 const BillingCycle = require('./billingCycle')
-const billingCycle = require('./billingCycle')
-
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
-BillingCycle.updateOptions({new: true, tunValidators: true})
-
-BillingCycle.route('cont', function(req, res, next) {
-    billingCycle.count(function(error, value) {
-        if(error) {
-        res.status(500).json({errors: [error]})
-        }else{
-            res.json({value})
+BillingCycle.updateOptions({ new: true, runValidators: true })
+BillingCycle.after('post', sendErrorsOrNext).after('put', sendErrorsOrNext)
+function sendErrorsOrNext(req, res, next) {
+    const bundle = res.locals.bundle
+    if (bundle.errors) {
+        const errors = parseErrors(bundle.errors)
+        res.status(500).json({ errors })
+    } else {
+        next()
+    }
+}
+function parseErrors(nodeRestfulErrors) {
+    const errors = []
+    _.forIn(nodeRestfulErrors, error => errors.push(error.message))
+    return errors
+}
+BillingCycle.route('count', function (req, res, next) {
+    BillingCycle.count(function (error, value) {
+        if (error) {
+            res.status(500).json({ errors: [error] })
+        } else {
+            res.json({ value })
         }
     })
 })
-
 module.exports = BillingCycle
