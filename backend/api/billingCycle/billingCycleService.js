c2fb4f3c2d8df86f4f0f2939efbcc4ba0b878ca9
Rafael <agostinetti@windowslive.com>
2020-08-04 17:19:40 -0300

    Sumario de pagamentos

--- backend/api/billingCycle/billingCycleService.js
+++ backend/api/billingCycle/billingCycleService.js
@@ -1,5 +1,17 @@
 const BillingCycle = require('./billingCycle')
+const billingCycle = require('./billingCycle')
 
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
+BillingCycle.updateOptions({new: true, tunValidators: true})
+
+BillingCycle.route('cont', function(req, res, next) {
+    billingCycle.count(function(error, value) {
+        if(error) {
+        res.status(500).json({errors: [error]})
+        }else{
+            res.json({value})
+        }
+    })
+})
 
 module.exports = BillingCycle
--- backend/config/database.js
+++ backend/config/database.js
@@ -1,4 +1,2 @@
 const mongoose = require('mongoose')
-module.exports = mongoose.connect('mongodb://192.168.0.104/db_finance')
-
-mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
+module.exports = mongoose.connect('mongodb://<root>:<72446066>192.168.0.104:27017/db_finance')
