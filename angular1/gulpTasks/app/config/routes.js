42acb9fb993abb6ab9968fcf1071f3cd92472601
Rafael <agostinetti@windowslive.com>
2020-08-05 20:42:38 -0300

    Configurando rotas com UI Router

---
+++ angular1/gulpTasks/app/config/routes.js
@@ -0,0 +1,14 @@
+angular.module("pimeiraApp").config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function ($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('dashboard', {
+            url: "/dashboard",
+            templateUrl: "dashboard/dashboard.html"
+        }).state('billingCycle', {
+            url: "/billingCycles",
+            templateUrl: "billingCycle/tabs.html"
+        })
+        $urlRouterProvider.otherwise('/dashboard')
+    }
+])
